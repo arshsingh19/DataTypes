5739f3f5e57a00a35c6cee4ffed58c28db0efaf8
arshsingh19 <arshdeep1177.cse16@chitkara.edu.in>
2018-09-05 09:12:12 +0530

    assignment 12

--- assignment 12/assingment 12.py
+++ assignment 12/assingment 12.py
@@ -10,14 +10,14 @@ print("Today's date is :-",today)#prting date
 
 #Ques_2. playing a video in browser
 import webbrowser
-webbrowser.open_new('https://www.youtube.com/watch?v=nceqQyqIa5o')#opening a youtube video in internet explorer
+webbrowser.open_new('https://www.youtube.com/watch?v=_3b6LZ2xAVs&t=627s')#opening a youtube video in internet explorer
 
 
 
 #Ques_3. changing the file extension in a directory
 import os
 Dir=os.listdir()
-for i in range(1,len(Dir)-1):
+for i in range(1,len(Dir)):
     Dir[i],ext=os.path.splitext(Dir[i])
-    os.rename(Dir[i]+'.txt',Dir[i]+'.jpg')
+    os.rename(Dir[i]+'.txt',Dir[i]+'changed.jpg')
 print(Dir)
